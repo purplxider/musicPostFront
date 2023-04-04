2a56c4bb4fe11a32a399ac32d3ec56d2dfccbb6c
yesiamok <jaehyunok@cau.ac.kr>
2023-04-05 04:26:29 +0900

    layout 변경

--- app/src/main/java/com/example/musicpost/MainActivity.java
+++ app/src/main/java/com/example/musicpost/MainActivity.java
@@ -2,32 +2,18 @@ package com.example.musicpost;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView titleLabel;
-    TextView shortContentLabel;
-    TextView currentLocationLabel;
-    Button addPostButton;
-    Button settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        titleLabel = (TextView) findViewById(R.id.titleLabel);
-        shortContentLabel = (TextView) findViewById(R.id.shortContentLabel);
-        currentLocationLabel = (TextView) findViewById(R.id.currentLocationLabel);
-        addPostButton = (Button) findViewById(R.id.addPostButton);
-        settingsButton = (Button) findViewById(R.id.settingsButton);
 
-        titleLabel.setText("Title");
-        shortContentLabel.setText("short content");
-        currentLocationLabel.setText("현재 위치");
-        addPostButton.setText("+");
-        settingsButton.setText("Settings");
     }
 }
