bb074f801206a8efbb4a42b4152aafdff5f80953
yesiamok <jaehyunok@cau.ac.kr>
2023-05-06 01:57:45 +0900

    화면 전환 애니메이션 추가

--- app/src/main/java/com/example/musicpost/MainActivity.java
+++ app/src/main/java/com/example/musicpost/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.musicpost;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -14,11 +14,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.security.MessageDigest;
-
 public class MainActivity extends AppCompatActivity {
     TextView locationLabel;
     RelativeLayout currentPostCard;
+    RelativeLayout nextPostCard;
     TextView titleLabel;
     TextView shortContentLabel;
     Button musicPlayButton;
@@ -53,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
         bindComponents(); // 화면에 있는 component 가져오기
         setEventListeners(); // 이벤트 리스너 설정
-        getAppKeyHash();
     }
 
     public void bindComponents() {
@@ -71,21 +69,107 @@ public class MainActivity extends AppCompatActivity {
     public void setEventListeners() {
         addPostButton.setOnClickListener(postClickListener);
         currentPostCard.setOnClickListener(detailedClickListener);
+        currentPostCard.setOnTouchListener(cardFlipListener);
     }
 
-    private void getAppKeyHash() {
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
+    View.OnTouchListener cardFlipListener = new View.OnTouchListener() {
+        private boolean isDragging = false;
+        private float startX;
+        private float startY;
+        private float currentX;
+        private float currentY;
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    // Save the start X and Y coordinates of the touch
+                    startX = event.getX();
+                    startY = event.getY();
+                    return true;
+                case MotionEvent.ACTION_MOVE:
+                    // Calculate the amount of drag in the X and Y directions
+                    currentX = event.getX();
+                    currentY = event.getY();
+                    float deltaX = currentX - startX;
+                    float deltaY = currentY - startY;
 
-                String something = Base64.encodeToString(md.digest(), Base64.NO_WRAP);
-                Log.e("Debug key", something);
+                    if (!isDragging && Math.abs(deltaX) > Math.abs(deltaY)) {
+                        // If the drag is mostly in the horizontal direction, start the curl animation
+                        isDragging = true;
+                        if (deltaX < 0) startLeftCurlAnimation();
+                        else startRightCurlAnimation();
+                        return true;
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    // Reset the dragging flag and touch event coordinates
+                    isDragging = false;
+                    startX = startY = currentX = currentY = 0f;
+                    break;
             }
-        } catch (Exception e) {
-            Log.e("Not found", e.toString());
+            return false;
         }
+    };
+
+    private void startLeftCurlAnimation() {
+        currentPostCard.setPivotX(0);
+        currentPostCard.setPivotY(0);
+        // Create an ObjectAnimator to animate the page's curl
+        ObjectAnimator animY = ObjectAnimator.ofFloat(currentPostCard, "rotationY", 0, -90);
+        ObjectAnimator animX = ObjectAnimator.ofFloat(currentPostCard, "rotationX", 0, 60);
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.playTogether(animX, animY);
+        animSet.setDuration(650);
+
+        animSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                // remove the old view
+                RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.mainLayout);
+                mainLayout.removeView(currentPostCard);
+                RelativeLayout newPostCard = createNewCard();
+                currentPostCard = newPostCard;
+                mainLayout.addView(currentPostCard);
+                currentPostCard.setOnTouchListener(cardFlipListener);
+            }
+        });
+
+        // Start the animation
+        animSet.start();
+
+    }
+
+    private void startRightCurlAnimation() {
+        currentPostCard.setPivotX(currentPostCard.getWidth());
+        currentPostCard.setPivotY(0);
+        // Create an ObjectAnimator to animate the page's curl
+        ObjectAnimator animY = ObjectAnimator.ofFloat(currentPostCard, "rotationY", 0, 90);
+        ObjectAnimator animX = ObjectAnimator.ofFloat(currentPostCard, "rotationX", 0, 60);
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.playTogether(animX, animY);
+        animSet.setDuration(650);
+
+        animSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                // remove the old view
+                RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.mainLayout);
+                mainLayout.removeView(currentPostCard);
+                RelativeLayout newPostCard = createNewCard();
+                currentPostCard = newPostCard;
+                mainLayout.addView(currentPostCard);
+                currentPostCard.setOnTouchListener(cardFlipListener);
+            }
+        });
+
+        // Start the animation
+        animSet.start();
+    }
+
+    private RelativeLayout createNewCard() {
+        RelativeLayout newCard = new RelativeLayout(this);
+        return newCard;
     }
 }
 
